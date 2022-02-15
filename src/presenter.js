7a69672c463edad7d5ff74bc2156fc0ec871710c
Marco <marco.escobar@ucb.edu.bo>
2022-02-15 09:03:46 -0400

    Multiplicador

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,28 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador"
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const num1 = document.querySelector("#numero-1");
+const num2 = document.querySelector("#numero-2");
+const multi = document.querySelector("#multi-form");
+const resp = document.querySelector("#resultado-resp");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
+  
+  div.innerHTML = "<p> La sumas es: " + sumar(firstNumber, secondNumber) + "</p>";      
+});
+multi.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const firstNumber1 = Number.parseInt(num1.value);  
+  const secondNumber2 = Number.parseInt(num2.value);
+  
+  resp.innerHTML = "<p>La multiplicación es: " + multiplicar(firstNumber1, secondNumber2) + "</p>";      
 });
