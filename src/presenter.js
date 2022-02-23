32f7388be2de92cded5f07c6e645f7ba4f8e6975
Marco <marco.escobar@ucb.edu.bo>
2022-02-23 07:45:12 -0400

    Segunda Funcionalidad

--- src/presenter.js
+++ src/presenter.js
@@ -2,10 +2,21 @@ const cant = document.querySelector("#cantidad-item");
 const form = document.querySelector("#cantidad-form");
 const div = document.querySelector("#resultado-div");
 
+const precio = document.querySelector("#precio-item");
+const prec = document.querySelector("#precio-form");
+const resp1 = document.querySelector("#resultado-prec");
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(cant.value);
   
   div.innerHTML = "<p> La cantidad es: " + firstNumber + "</p>";      
+});
+prec.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const SecondNumber = Number.parseInt(precio.value);
+  
+  resp1.innerHTML = "<p> La PrEcio es: " + SecondNumber + "</p>";      
 });
