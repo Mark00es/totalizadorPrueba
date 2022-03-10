d1632055958fe6943af006e755a19ba8bf997b8d
Marco <marco.escobar@ucb.edu.bo>
2022-03-10 08:59:09 -0400

    ImpuestosImplementados

--- src/estado.js
+++ src/estado.js
@@ -1,16 +1,23 @@
 function mostrarImpu(a) {
     var resp = 0;
-    if(a == "CA")
-        resp=8.25;
-    if(a == "TX")
-        resp = 6.25;        
-    if(a == "AL")
-        resp=4.00;
-    if(a == "NV")
-        resp=8.00;    
-    if(a =="UT")
-        resp = 6.65;
+    switch(a){
+        case "UT":
+            resp = 6.65;
+            break;
+        case "NV":
+            resp = 8.00;
+            break;
+        case "TX":
+            resp = 6.25;
+            break;
+        case "AL":
+            resp = 4.00;
+            break;
+        case "CA":
+            resp = 8.25;
+            break;
+    }
     return resp;
-  }
+}
   
-  export default mostrarImpu;
+export default mostrarImpu;
