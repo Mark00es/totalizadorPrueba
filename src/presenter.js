041969a93bb55e3968d3a6c1661ba5ee92a56c94
Marco <marco.escobar@ucb.edu.bo>
2022-03-10 08:45:42 -0400

    TerceraFuncionalidad

--- src/presenter.js
+++ src/presenter.js
@@ -1,22 +1,18 @@
 const cant = document.querySelector("#cantidad-item");
-const form = document.querySelector("#cantidad-form");
-const div = document.querySelector("#resultado-div");
-
 const precio = document.querySelector("#precio-item");
-const prec = document.querySelector("#precio-form");
-const resp1 = document.querySelector("#resultado-prec");
+const form = document.querySelector("#cantidad-form");
+const estado = document.querySelector("#estado-item");
+const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(cant.value);
-  
-  div.innerHTML = "<p> La cantidad es: " + firstNumber + "</p>";      
-});
-prec.addEventListener("submit", (event) => {
-  event.preventDefault();
-
   const SecondNumber = Number.parseInt(precio.value);
-  
-  resp1.innerHTML = "<p> La PrEcio es: " + SecondNumber + "</p>";      
+  const est = estado.value;
+
+  div.innerHTML = "<p>Cantidad de item: " + firstNumber + "</p>";      
+  div.innerHTML = div.innerHTML +  "<p>Precio por Item: " + SecondNumber + "</p>";
+  div.innerHTML = div.innerHTML +  "<p>Codigo de estado: " + est + "</p>";
+
 });
