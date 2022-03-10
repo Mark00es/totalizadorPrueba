205e785e24772701e5276d14c281bc15fd450c85
Marco <marco.escobar@ucb.edu.bo>
2022-03-10 09:03:33 -0400

    Descuento3000

--- src/Descuento.js
+++ src/Descuento.js
@@ -1,7 +1,9 @@
 function mostrarDescu(a) {
     var resp = 0;        
     if(a>=1000 && a<3000)
-        resp = 3.00;     
+        resp = 3.00; 
+    else if(a>=3000 && a<7000)
+        resp = 5.00;      
     return resp;
   }
 
