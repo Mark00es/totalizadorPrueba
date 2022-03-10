fcd7b089dfb902cd78ee5ec28114d50324fabe23
Marco <marco.escobar@ucb.edu.bo>
2022-03-10 08:57:46 -0400

    ImpuestoUT

--- src/estado.js
+++ src/estado.js
@@ -1,17 +1,15 @@
 function mostrarImpu(a) {
     var resp = 0;
-    if(a == "CA"){
+    if(a == "CA")
         resp=8.25;
-    }
-    if(a == "TX"){
+    if(a == "TX")
         resp = 6.25;        
-    }
-    if(a="AL"){
+    if(a == "AL")
         resp=4.00;
-    }
-    if(a="NV"){
-        resp=8.00;
-    }
+    if(a == "NV")
+        resp=8.00;    
+    if(a =="UT")
+        resp = 6.65;
     return resp;
   }
   
