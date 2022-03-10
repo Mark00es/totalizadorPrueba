0555573a36294a35d5e335367fd2bc2498c92816
Marco <marco.escobar@ucb.edu.bo>
2022-03-10 08:54:46 -0400

    ImpuestoTX

--- src/estado.js
+++ src/estado.js
@@ -1,12 +1,12 @@
-function calcularImpuesto(a) {
+function mostrarImpu(a) {
     var resp = 0;
-    if(a == "TX"){
-            resp = 6.25;        
-    }
     if(a == "CA"){
         resp=8.25;
+    }
+    if(a == "TX"){
+        resp = 6.25;        
     }
     return resp;
   }
   
-  export default calcularImpuesto;
+  export default mostrarImpu;
